b64276df1033d7d39644d47c752f51d624366439
yuchan1120 <nogginpanic@gmail.com>
2022-09-05 23:49:12 +0900

    postsコントローラを修正

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -4,8 +4,8 @@ module Api
       before_action :set_post, only: [:show, :update, :destroy]
 
       def index
-        posts = Post.order(created_at: :desc)
-        render json: { status: 'SUCCESS', message: 'Loaded posts', data: posts }
+        @post = Post.all
+        render json: @post
       end
 
       def show
